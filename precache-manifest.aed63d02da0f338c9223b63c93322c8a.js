cd5be4df32fef252f0c9129b1b1253263d568d8b
NLegge <little.legge@gmail.com>
2020-07-15 15:00:57 -0700

    Updates

--- precache-manifest.aed63d02da0f338c9223b63c93322c8a.js
+++ precache-manifest.aed63d02da0f338c9223b63c93322c8a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d57056b0d27588b2adfab2269d771a4",
+    "revision": "96a4b942622f99add5b017f38c1be9f8",
     "url": "/random-movie-picker/index.html"
   },
   {
-    "revision": "35f3fb80507c8d1dc77e",
+    "revision": "f00cfed1d38eaad471e8",
     "url": "/random-movie-picker/static/css/main.459f7dce.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/random-movie-picker/static/js/2.6a0f500c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35f3fb80507c8d1dc77e",
-    "url": "/random-movie-picker/static/js/main.21b343e5.chunk.js"
+    "revision": "f00cfed1d38eaad471e8",
+    "url": "/random-movie-picker/static/js/main.bb1b9380.chunk.js"
   },
   {
     "revision": "83cd9024dde54adcd3f0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-movie-picker/precache-manifest.89b23679fa2eba71396f8878ce4a67dc.js"
+  "/random-movie-picker/precache-manifest.aed63d02da0f338c9223b63c93322c8a.js"
 );
 
 self.addEventListener('message', (event) => {
